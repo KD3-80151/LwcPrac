440e360b5bec560dd4161e85f3899d83a1292a38
KD3-80151 <sagarkumarchhotu@ymail.com>
2025-09-09 18:03:37 +0530

    new changes

---
+++ force-app/main/default/lwc/dynamicFileUploader/dynamicFileUploader.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class DynamicFileUploader extends LightningElement {
+    
+}
---
+++ force-app/main/default/lwc/showToastExampleComponent/showToastExampleComponent.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class ShowToastExampleComponent extends LightningElement {]
+    @api toastTitle;
+    @api toastMessage;
+
+    @api invoke(){
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: this.toastTitle,
+                message: this.toastMessage,
+            }),
+        );
+    }
+}
